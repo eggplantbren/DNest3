f407cd91681d3708fbab6d1a7149ed5eb1312eae
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-17 00:38:05 -0700

    Fleshing out TestModel - need a global RNG

--- TestModel.cpp
+++ TestModel.cpp
@@ -1,2 +1,35 @@
 #include "TestModel.h"
 
+using namespace std;
+
+namespace NewSampler
+{
+
+TestModel::TestModel()
+:params(20)
+{
+
+}
+
+void TestModel::fromPrior()
+{
+
+}
+
+double TestModel::perturb()
+{
+	return 0.;
+}
+
+double TestModel::logLikelihood() const
+{
+	return 0.;
+}
+
+void TestModel::print(std::ostream& out) const
+{
+	out<<' ';
+}
+
+} // namespace TestModel
+
--- TestModel.h
+++ TestModel.h
@@ -2,12 +2,30 @@
 #define _TestModel_
 
 #include "Model.h"
+#include <vector>
 
 namespace NewSampler
 {
 
 class TestModel:public Model
 {
+	private:
+		std::vector<double> params;
+
+	public:
+		TestModel();
+
+		// Generate the point from the prior
+		void fromPrior();
+
+		// Metropolis-Hastings proposals
+		double perturb();
+
+		// Likelihood function
+		double logLikelihood() const;
+
+		// Print to stream
+		void print(std::ostream& out) const;
 
 
 };
