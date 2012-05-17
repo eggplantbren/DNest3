9d352e716c7592fd9bb74778ed4d8d3b456afa56
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-17 15:20:53 -0700

    Made variables private and some stuff friend

--- LikelihoodType.h
+++ LikelihoodType.h
@@ -1,17 +1,31 @@
 #ifndef _LikelihoodType_
 #define _LikelihoodType_
 
+#include <istream>
+#include <ostream>
+
 namespace DNest3
 {
 
+// Forward declaration
+class Level;
+
 class LikelihoodType
 {
-	public:
+	private:
 		double logL;
 		double tieBreaker;
 
+	public:
 		LikelihoodType();
 		LikelihoodType(double logL, double tieBreaker);
+
+	// Various stuff needs to be a friend
+	friend class Level;
+	template<class ModelType> friend class Sampler;
+	friend bool operator < (const LikelihoodType& l1, const LikelihoodType& l2);
+	friend std::ostream& operator << (std::ostream& out, const Level& level);
+	friend std::istream& operator >> (std::istream& in, Level& level);
 };
 
 bool operator < (const LikelihoodType& l1, const LikelihoodType& l2);
