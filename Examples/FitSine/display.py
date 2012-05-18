5219c943a46c037d09764cfdef0e89650ecc27dd
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-18 16:44:39 -0700

    Nice inference on number of components

--- Examples/FitSine/display.py
+++ Examples/FitSine/display.py
@@ -26,6 +26,8 @@ start += maxNumComponents
 phases = sample[:, start:start+maxNumComponents]
 start += maxNumComponents
 
+numComponents = (amplitudes > 0.).sum(axis=1)
+
 plt.ion()
 for i in xrange(0, sample.shape[0]):
 	
@@ -33,6 +35,7 @@ for i in xrange(0, sample.shape[0]):
 	for j in xrange(0, maxNumComponents):
 		mockData += amplitudes[i, j]*np.sin(2*np.pi*frequencies[i, j]*data[:,0] + phases[i, j])
 
+	plt.subplot(2,1,1)
 	plt.hold(False)
 	plt.errorbar(data[:,0], data[:,1], yerr=data[:,2], fmt='bo')
 	plt.hold(True)
@@ -41,6 +44,13 @@ for i in xrange(0, sample.shape[0]):
 	plt.xlabel('Time')
 	plt.ylabel('y')
 	plt.title(i+1)
+
+	plt.subplot(2,1,2)
+	plt.hold(False)
+	num = numComponents[0:(i+1)]
+	plt.hist(num, bins=[i for i in xrange(0, num.max()+1)], align='left', rwidth=0.3)
+	plt.xlabel('Number of Components')
+	plt.ylabel('Probability')
 	plt.draw()
 
 plt.ioff()
