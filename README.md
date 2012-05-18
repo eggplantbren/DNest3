a17c4e9d6547abb602551ff30a577d416f01cd7a
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-17 18:14:24 -0700

    Added GPL headers

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ DNest 3
 
 This is a reimplementation of Diffusive Nested Sampling, since my C++ skills
 have improved. It has improved performance (in terms of the sampling overhead, 
-I still expect likelihood evaluations to dominate) and is cleaner code.
-It has almost all of the features that the older DNest has.
+I still expect likelihood evaluations to dominate) and is cleaner code:
+implementing new models should be even easier.
 
-(c) 2012 Brendon J. Brewer. Licence: GNU GPL 3.
+It has almost all of the features that the older DNest has. Currently missing
+features include loading of a levels.txt file, and multithreading support.
+
+(c) 2009-2012 Brendon J. Brewer. Licence: GNU GPL 3.
 
 
--- showresults.py
+++ showresults.py
@@ -1,24 +1,19 @@
+# Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
 #
-#    Copyright (C) 2011, 2012 Brendon J. Brewer
-#    This file is part of DNest, the Diffusive Nested Sampler.
+# This file is part of DNest.
 #
-#    DNest is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# DNest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    DNest is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# DNest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
-#    along with DNest.  If not, see <http://www.gnu.org/licenses/>.
-
-import matplotlib
-
-# Uncomment on Linux if figures don't draw until the end (for numResampleLogX > 1)
-#matplotlib.use('GTkAgg')
+# You should have received a copy of the GNU General Public License
+# along with DNest. If not, see <http://www.gnu.org/licenses/>.
 
 from copy import deepcopy
 from pylab import *
