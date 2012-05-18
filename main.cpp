3f291249735d2e18ef74b22daab87f319adcb510
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-17 17:16:17 -0700

    Seed the RNG with the time

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 #include "Sampler.h"
 #include "Models/SpikeSlab/SpikeSlab.h"
 #include "RandomNumberGenerator.h"
@@ -9,6 +10,7 @@ using namespace DNest3;
 int main()
 {
 	Options options("OPTIONS");
+	RandomNumberGenerator::get_instance().setSeed(-time(0));
 	Sampler<SpikeSlab> sampler(options);
 	sampler.run();
 	return 0;
