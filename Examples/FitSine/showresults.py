589a6c0f40bc7e1ddea704e6c402ff5126b4c589
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-20 19:16:58 -0700

    Cleaned up showresults

--- Examples/FitSine/showresults.py
+++ Examples/FitSine/showresults.py
@@ -1,91 +1,85 @@
-# Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
-#
-# This file is part of DNest.
-#
-# DNest is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DNest is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DNest. If not, see <http://www.gnu.org/licenses/>.
-
-from copy import deepcopy
-from pylab import *
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
 
 def logsumexp(values):
-	biggest = max(values)
+	biggest = np.max(values)
 	x = values - biggest
-	result = log(sum(exp(x))) + biggest
+	result = np.log(np.sum(np.exp(x))) + biggest
 	return result
 
 def logdiffexp(x1, x2):
 	biggest = x1
 	xx1 = x1 - biggest
 	xx2 = x2 - biggest
-	result = log(exp(xx1) - exp(xx2)) + biggest
+	result = np.log(np.exp(xx1) - np.exp(xx2)) + biggest
 	return result
 
 
 numResampleLogX = 1
 temperature = 1.0
 
-levels = atleast_2d(loadtxt("levels.txt"))
-sample_info = atleast_2d(loadtxt("sample_info.txt"))
-sample = atleast_2d(loadtxt("sample.txt"))
+levels = np.atleast_2d(np.loadtxt("levels.txt"))
+sample_info = np.atleast_2d(np.loadtxt("sample_info.txt"))
+sample = np.atleast_2d(np.loadtxt("sample.txt"))
 
 if sample.shape[0] != sample_info.shape[0]:
 	print('ERROR. Size mismatch. The usual cause is that you\'re still running DNest and it wrote to the files that showresults loads. Try suspending DNest temporarily.')
 	exit()
 
-ion()
-figure(1)
-plot(sample_info[:,0])
-xlabel("Iteration")
-ylabel("Level")
-draw()
+if numResampleLogX > 1:
+	plt.ion()
 
-figure(2)
-subplot(2,1,1)
-plot(diff(levels[:,0]))
-ylabel("Compression")
-xlabel("Level")
-subplot(2,1,2)
-plot(levels[:,3]/levels[:,4])
-ylim([0, 1])
-xlabel("Level")
-ylabel("MH Acceptance")
-draw()
+plt.figure(1)
+plt.plot(sample_info[:,0])
+plt.xlabel("Iteration")
+plt.ylabel("Level")
+if numResampleLogX > 1:
+	plt.draw()
+
+plt.figure(2)
+plt.subplot(2,1,1)
+plt.plot(np.diff(levels[:,0]))
+plt.ylabel("Compression")
+plt.xlabel("Level")
+xlim = plt.gca().get_xlim()
+if numResampleLogX > 1:
+	plt.draw()
+
+plt.subplot(2,1,2)
+good = np.nonzero(levels[:,4] > 0)[0]
+plt.plot(levels[good,3]/levels[good,4])
+plt.xlim(xlim)
+plt.ylim([-0.1, 1.1])
+plt.xlabel("Level")
+plt.ylabel("MH Acceptance")
+if numResampleLogX > 1:
+	plt.draw()
 
 # Convert to lists of tuples
-logl_levels = [(levels[i,1], levels[i, 2]) for i in range(0, levels.shape[0])] # logl, tiebreaker
-logl_samples = [(sample_info[i, 1], sample_info[i, 2], i) for i in range(0, sample.shape[0])] # logl, tiebreaker, id
-logx_samples = zeros((sample_info.shape[0], numResampleLogX))
-logp_samples = zeros((sample_info.shape[0], numResampleLogX))
-logP_samples = zeros((sample_info.shape[0], numResampleLogX))
-P_samples = zeros((sample_info.shape[0], numResampleLogX))
-logz_estimates = zeros((numResampleLogX, 1))
-H_estimates = zeros((numResampleLogX, 1))
+logl_levels = [(levels[i,1], levels[i, 2]) for i in xrange(0, levels.shape[0])] # logl, tiebreaker
+logl_samples = [(sample_info[i, 1], sample_info[i, 2], i) for i in xrange(0, sample.shape[0])] # logl, tiebreaker, id
+logx_samples = np.zeros((sample_info.shape[0], numResampleLogX))
+logp_samples = np.zeros((sample_info.shape[0], numResampleLogX))
+logP_samples = np.zeros((sample_info.shape[0], numResampleLogX))
+P_samples = np.zeros((sample_info.shape[0], numResampleLogX))
+logz_estimates = np.zeros((numResampleLogX, 1))
+H_estimates = np.zeros((numResampleLogX, 1))
 
 # Find sandwiching level for each sample
 sandwich = sample_info[:,0].copy().astype('int')
-for i in range(0, sample.shape[0]):
+for i in xrange(0, sample.shape[0]):
 	while sandwich[i] < levels.shape[0]-1 and logl_samples[i] > logl_levels[sandwich[i] + 1]:
 		sandwich[i] += 1
 
-for z in range(0, numResampleLogX):
+for z in xrange(0, numResampleLogX):
 	# For each level
 	for i in range(0, levels.shape[0]):
 		# Find the samples sandwiched by this level
-		which = nonzero(sandwich == i)[0]
+		which = np.nonzero(sandwich == i)[0]
 		logl_samples_thisLevel = [] # (logl, tieBreaker, ID)
-		for j in range(0, len(which)):
-			logl_samples_thisLevel.append(deepcopy(logl_samples[which[j]]))
+		for j in xrange(0, len(which)):
+			logl_samples_thisLevel.append(copy.deepcopy(logl_samples[which[j]]))
 		logl_samples_thisLevel = sorted(logl_samples_thisLevel)
 		N = len(logl_samples_thisLevel)
 
@@ -95,18 +89,18 @@ for z in range(0, numResampleLogX):
 			logx_min = -1E300
 		else:
 			logx_min = levels[i+1, 0]
-		Umin = exp(logx_min - logx_max)
+		Umin = np.exp(logx_min - logx_max)
 
 		if N == 0 or numResampleLogX > 1:
-			U = Umin + (1.0 - Umin)*rand(len(which))
+			U = Umin + (1. - Umin)*np.random.rand(len(which))
 		else:
-			U = Umin + (1.0 - Umin)*linspace(1.0/(N+1), 1.0 - 1.0/(N+1), N)
-		logx_samples_thisLevel = sort(logx_max + log(U))[::-1]
+			U = Umin + (1. - Umin)*np.linspace(1./(N+1), 1. - 1./(N+1), N)
+		logx_samples_thisLevel = np.sort(logx_max + np.log(U))[::-1]
 
-		for j in range(0, len(which)):
+		for j in xrange(0, which.size):
 			logx_samples[logl_samples_thisLevel[j][2]][z] = logx_samples_thisLevel[j]
 
-			if j != len(which)-1:
+			if j != which.size - 1:
 				left = logx_samples_thisLevel[j+1]
 			elif i == levels.shape[0]-1:
 				left = -1E300
@@ -118,7 +112,7 @@ for z in range(0, numResampleLogX):
 			else:
 				right = levels[i][0]
 
-			logp_samples[logl_samples_thisLevel[j][2]][z] = log(0.5) + logdiffexp(right, left)
+			logp_samples[logl_samples_thisLevel[j][2]][z] = np.log(0.5) + logdiffexp(right, left)
 
 	logl = sample_info[:,1]/temperature
 
@@ -126,30 +120,38 @@ for z in range(0, numResampleLogX):
 	logP_samples[:,z] = logp_samples[:,z] + logl
 	logz_estimates[z] = logsumexp(logP_samples[:,z])
 	logP_samples[:,z] -= logz_estimates[z]
-	P_samples[:,z] = exp(logP_samples[:,z])
-	H_estimates[z] = -logz_estimates[z] + sum(P_samples[:,z]*logl)
+	P_samples[:,z] = np.exp(logP_samples[:,z])
+	H_estimates[z] = -logz_estimates[z] + np.sum(P_samples[:,z]*logl)
 
-	figure(3)
-	clf()
-	subplot(2,1,1)
-	p1 = plot(logx_samples[:,z], sample_info[:,1], 'b.', label='Samples')
-	p2 = plot(levels[1:,0], levels[1:,1], 'r.', label='Levels')
-	legend(numpoints=1, loc='lower left')
-	ylabel('log(L)')
-	title(str(z+1) + "/" + str(numResampleLogX) + ", log(Z) = " + str(logz_estimates[z][0]))
-	subplot(2,1,2)
-	plot(logx_samples[:,z], P_samples[:,z], 'b.')
-	ylabel('Posterior Weights')
-	xlabel('log(X)')
-	draw()
+	plt.figure(3)
+	if z == 0:
+		plt.subplot(2,1,1)
+		plt.plot(logx_samples[:,z], sample_info[:,1], 'b.', label='Samples')
+		plt.hold(True)
+		plt.plot(levels[1:,0], levels[1:,1], 'r.', label='Levels')
+		plt.legend(numpoints=1, loc='lower left')
+		plt.ylabel('log(L)')
+		plt.title(str(z+1) + "/" + str(numResampleLogX) + ", log(Z) = " + str(logz_estimates[z][0]))
+		if numResampleLogX > 1:
+			plt.draw()
+		xlim = plt.gca().get_xlim()
 
-P_samples = mean(P_samples, 1)
-P_samples = P_samples/sum(P_samples)
-logz_estimate = mean(logz_estimates)
-logz_error = std(logz_estimates)
-H_estimate = mean(H_estimates)
-H_error = std(H_estimates)
-ESS = exp(-sum(P_samples*log(P_samples+1E-300)))
+	plt.subplot(2,1,2)
+	plt.hold(False)
+	plt.plot(logx_samples[:,z], P_samples[:,z], 'b.')
+	plt.ylabel('Posterior Weights')
+	plt.xlabel('log(X)')
+	plt.xlim(xlim)
+	if numResampleLogX > 1:
+		plt.draw()
+
+P_samples = np.mean(P_samples, 1)
+P_samples = P_samples/np.sum(P_samples)
+logz_estimate = np.mean(logz_estimates)
+logz_error = np.std(logz_estimates)
+H_estimate = np.mean(H_estimates)
+H_error = np.std(H_estimates)
+ESS = np.exp(-np.sum(P_samples*np.log(P_samples+1E-300)))
 
 print("log(Z) = " + str(logz_estimate) + " +- " + str(logz_error))
 print("Information = " + str(H_estimate) + " +- " + str(H_error) + " nats.")
@@ -157,20 +159,20 @@ print("Effective sample size = " + str(ESS))
 
 # Resample to uniform weight
 N = int(ESS)
-posterior_sample = zeros((N, shape(sample)[1]))
+posterior_sample = np.zeros((N, sample.shape[1]))
 w = P_samples
-w = w/max(w)
-savetxt('weights.txt', w) # Save weights
-for i in range(0, N):
+w = w/np.max(w)
+np.savetxt('weights.txt', w) # Save weights
+for i in xrange(0, N):
 	while True:
-		which = randint(sample.shape[0])
-		if rand() <= w[which]:
+		which = np.random.randint(sample.shape[0])
+		if np.random.rand() <= w[which]:
 			break
 	posterior_sample[i,:] = sample[which,:]
-savetxt("posterior_sample.txt", posterior_sample)
+np.savetxt("posterior_sample.txt", posterior_sample)
 
-ioff()
-show()
+if numResampleLogX > 1:
+	plt.ioff()
+plt.show()
 
 import display
-
