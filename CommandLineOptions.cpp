8f383b07dbe52c88aad60a67c887ac6f916e501f
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-21 11:36:24 -0700

    Updated more copyright statements

--- CommandLineOptions.cpp
+++ CommandLineOptions.cpp
@@ -1,3 +1,22 @@
+/*
+* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
+*
+* This file is part of DNest3.
+*
+* DNest3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* DNest3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "CommandLineOptions.h"
 #include <ctime>
 #include <sstream>
--- Utils.cpp
+++ Utils.cpp
@@ -1,20 +1,20 @@
 /*
 * Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
 *
-* This file is part of DNest.
+* This file is part of DNest3.
 *
-* DNest is free software: you can redistribute it and/or modify
+* DNest3 is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* DNest is distributed in the hope that it will be useful,
+* DNest3 is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with DNest. If not, see <http://www.gnu.org/licenses/>.
+* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "Utils.h"
