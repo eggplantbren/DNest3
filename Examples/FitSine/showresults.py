ac4737c63ebfd7f7cb1adaaa4572cd2f2a33d9bb
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-22 16:50:59 -0700

    postprocess is now a function

--- Examples/FitSine/showresults.py
+++ Examples/FitSine/showresults.py
@@ -22,5 +22,6 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 parentdir = os.path.dirname(parentdir)
 os.sys.path.insert(0, parentdir) 
 import postprocess
+postprocess.postprocess()
 import display
 
--- Examples/SpikeSlab/showresults.py
+++ Examples/SpikeSlab/showresults.py
@@ -22,4 +22,5 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 parentdir = os.path.dirname(parentdir)
 os.sys.path.insert(0, parentdir) 
 import postprocess
+postprocess.postprocess()
 
