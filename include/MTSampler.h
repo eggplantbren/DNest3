a557cc4f4664f93f8cb701fa200f20d3e3327d77
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-07 18:19:04 -0700

    Skeleton

---
+++ include/MTSampler.h
@@ -0,0 +1,19 @@
+#ifndef DNest3_MTSampler
+#define DNest3_MTSampler
+
+namespace DNest3
+{
+
+template<class ModelType>
+class MTSampler
+{
+
+
+};
+
+} // namespace DNest3
+
+#include "MTSamplerImpl.h"
+
+#endif
+
