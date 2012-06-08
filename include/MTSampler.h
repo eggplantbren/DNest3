a82ca20b9f05cbc0598e76302bb06bb829b64eee
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-08 08:43:50 -0700

    Sampler -->MTSampler

--- include/MTSampler.h
+++ include/MTSampler.h
@@ -1,9 +1,29 @@
-#ifndef DNest3_MTSampler
-#define DNest3_MTSampler
+/*
+* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
+*
+* This file is part of DNest3.
+*
+* DNest3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* DNest3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
+*/
 
-#include "Sampler.h"
+#ifndef DNest3_MTSampler_h
+#define DNest3_MTSampler_h
+
 #include <vector>
-#include <boost/thread.hpp>
+#include "LikelihoodType.h"
+#include "Options.h"
+#include "Level.h"
 
 namespace DNest3
 {
@@ -11,23 +31,37 @@ namespace DNest3
 template<class ModelType>
 class MTSampler
 {
+	// MTMTSamplers can manipulate MTSamplers
+	template<class Type>
+	friend class MTMTSampler;
+
 	private:
-		static const int skip;
+		// Options (most useful comment ever)
+		Options options;
 
-		boost::barrier* barrier;
+		// Stuff pertaining to the particles
+		std::vector<ModelType> particles;
+		std::vector<LikelihoodType> logL;
+		std::vector<int> indices;
 
-		std::vector< Sampler<ModelType> > samplers;
-		std::vector<Level> levels; // levels backup
+		// Stuff pertaining to the level structure
+		std::vector<Level> levels;
+		std::vector<LikelihoodType> logLKeep;
 
-		int numSaves;
-		long numStepsSinceSave; // Count number of steps since last save
+		// Whether initialise() has ever been called
+		bool initialised;
+
+		// Number of MCMC steps ever done
+		long count;
+
+		// Flag to indicate whether this is being used as
+		// a sampler in its own right or just as a helper for MTMTSampler
+		bool primary;
+
 
 	public:
 		// Constructor: Pass in Options object
-		MTSampler(int numThreads, const Options& options);
-
-		// Destructor - free heap memory
-		~MTSampler();
+		MTSampler(const Options& options);
 
 		// Load levels from file
 		void loadLevels(const char* filename);
@@ -37,17 +71,38 @@ class MTSampler
 
 	private:
 		// These are helper methods -- not part of the public interface
-		void runThread(int thread, unsigned long firstSeed);
 
-		void bookKeeping();
+		// Initialise all objects from the prior
+		void initialise();
 
-		void combineLevels();
+		// run() but only for a certain number of steps
+		void run(int steps);
 
+		// Choose a particle and do one step with it
+		// Return a flag - whether to continue or not
+		bool step();
 
+		// Do a M-H step of a particle
+		void updateParticle(int which);
 
+		// Do a M-H step of an index
+		void updateIndex(int which);
+
+		// Check for creation of a new level, saving of particles, etc
+		bool bookKeeping(int which);
+
+		// Savers
+		void saveParticle(int which) const;
+		void saveLevels() const;
+
+		// Pushing function
+		double logPush(int index) const;
+
+		// Delete any bad particles
+		void deleteParticle();
 };
 
-} // namespace DNest3
+}
 
 #include "MTSamplerImpl.h"
 
--- include/MTSamplerImpl.h
+++ include/MTSamplerImpl.h
@@ -1,128 +1,353 @@
-#include <boost/bind.hpp>
+/*
+* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
+*
+* This file is part of DNest3.
+*
+* DNest3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* DNest3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "RandomNumberGenerator.h"
+#include "Utils.h"
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
 
 namespace DNest3
 {
 
 template<class ModelType>
-const int MTSampler<ModelType>::skip = 1000;
-
-template<class ModelType>
-MTSampler<ModelType>::MTSampler(int numThreads, const Options& options)
-:barrier(new boost::barrier(numThreads))
-,samplers(numThreads, Sampler<ModelType>(options))
-,numSaves(0), numStepsSinceSave(0)
+MTSampler<ModelType>::MTSampler(const Options& options)
+:options(options)
+,particles(options.numParticles)
+,logL(options.numParticles)
+,indices(options.numParticles, 0)
+,levels(1, Level(0., -1E300, 0.))
+,logLKeep(0)
+,initialised(false)
+,count(0)
+,primary(true)
 {
-	levels = samplers[0].levels;
-	if(numThreads > 1)
-		for(size_t i=0; i<samplers.size(); i++)
-			samplers[i].primary = false;
-}
-
-template<class ModelType>
-MTSampler<ModelType>::~MTSampler()
-{
-	delete barrier;
+	logLKeep.reserve(2*options.newLevelInterval);
 }
 
 template<class ModelType>
 void MTSampler<ModelType>::loadLevels(const char* filename)
 {
-	samplers[0].loadLevels(filename);
-	for(size_t i=1; i<samplers.size(); i++)
-		samplers[i].levels = samplers[0].levels;
+	if(initialised)
+		std::cerr<<"# WARNING: Please load level structure before running sampler."<<std::endl;
+	levels = Level::loadLevels(filename);
+	if(static_cast<int>(levels.size()) > options.maxNumLevels)
+	{
+		std::cout<<"# Truncating to "<<options.maxNumLevels<<" levels."<<std::endl;
+		levels.erase(levels.begin() + options.maxNumLevels, levels.end());
+	} 
+
+	Level::renormaliseVisits(levels, options.newLevelInterval);
+	Level::recalculateLogX(levels, options.newLevelInterval);
+	saveLevels();
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::initialise()
+{
+	assert(!initialised);
+	std::cout<<"# Generating "<<options.numParticles<<
+			" particles from the prior..."<<std::flush;
+	for(int i=0; i<options.numParticles; i++)
+	{
+		particles[i].fromPrior();
+		logL[i] = LikelihoodType
+				(particles[i].logLikelihood(), randomU());
+		logLKeep.push_back(logL[i]);
+	}
+	initialised = true;
+	std::cout<<"done."<<std::endl;
 }
 
 template<class ModelType>
 void MTSampler<ModelType>::run()
 {
-	unsigned long firstSeed = RandomNumberGenerator::get_instance().
-					get_seed();
-
-	boost::thread_group threads;
-	for(size_t i=0; i<samplers.size(); i++)
-		threads.create_thread(boost::bind
-			(&MTSampler<ModelType>::runThread, this, i, firstSeed));
-	threads.join_all();
-}
-
-template<class ModelType>
-void MTSampler<ModelType>::runThread(int thread, unsigned long firstSeed)
-{
-	RandomNumberGenerator::initialise_instance();
-	RandomNumberGenerator::get_instance().set_seed(firstSeed + 10*thread);
-
-	if(samplers.size() == 1)
-	{
-		samplers[thread].run();
-		return;
-	}
+	if(!initialised)
+		initialise();
 
 	while(true)
 	{
-		samplers[thread].run(skip);
-		barrier->wait();
-		if(thread == 0)
-			bookKeeping();
-		barrier->wait();
+		bool cont = step();
+		if(!cont)
+			break;
 	}
 }
 
 template<class ModelType>
-void MTSampler<ModelType>::bookKeeping()
+void MTSampler<ModelType>::run(int steps)
 {
-	numStepsSinceSave += skip;
-	combineLevels();
+	if(!initialised)
+		initialise();
 
-	// Make one giant sampler
-	Sampler<ModelType> giant = samplers[0];
-	giant.options.numParticles *= samplers.size();
-
-	for(size_t i=1; i<samplers.size(); i++)
+	for(int i=0; i<steps; i++)
 	{
-		giant.particles.insert(giant.particles.end(),
-			samplers[i].particles.begin(), samplers[i].particles.end());
-		giant.logLKeep.insert(giant.logLKeep.end(),
-			samplers[i].logLKeep.begin(),
-			samplers[i].logLKeep.end());
-		samplers[i].logLKeep.clear();
-	}
-
-	if(numStepsSinceSave >= samplers[0].options.saveInterval)
-	{
-		// Fiddle with samplers[which] to fool it into thinking it's due to save
-		long old = giant.count;
-		giant.count = (numSaves+1)*samplers[0].options.saveInterval;
-		giant.bookKeeping(randInt(giant.particles.size()));
-		numSaves++;
-		numStepsSinceSave = 0;
-
-		// It may have added a level, grab it
-		samplers[0].logLKeep = giant.logLKeep;
-		levels = giant.levels;
-		for(size_t i=0; i<samplers.size(); i++)
-			samplers[i].levels = levels;
+		bool cont = step();
+		if(!cont)
+			break;
 	}
 }
 
 template<class ModelType>
-void MTSampler<ModelType>::combineLevels()
+bool MTSampler<ModelType>::step()
 {
-	std::vector<Level> old = levels;
-
-	// Extract level stats from all samplers
-	for(size_t i=0; i<levels.size(); i++)
+	// Move a particle
+	int which = randInt(options.numParticles);
+	if(randomU() <= 0.5)
 	{
-		for(size_t j=0; j<samplers.size(); j++)
+		updateParticle(which);
+		updateIndex(which);
+	}
+	else
+	{
+		updateIndex(which);
+		updateParticle(which);
+	}
+	count++;
+
+	// Accumulate visits, exceeds
+	int index = indices[which];
+	while(index < static_cast<int>(levels.size()) - 1)
+	{
+		bool exceeds = levels[index+1].get_cutoff() < logL[which];
+		levels[index].incrementVisits(exceeds);
+		if(!exceeds)
+			break;
+		index++;
+	}
+
+	// Accumulate likelihoods for making a new level
+	if(static_cast<int>(levels.size()) < options.maxNumLevels &&
+			levels.back().get_cutoff() < logL[which])
+		logLKeep.push_back(logL[which]);
+
+	bool cont = true;
+	if(primary)
+		cont = bookKeeping(which);
+	return cont;
+}
+
+template<class ModelType>
+bool MTSampler<ModelType>::bookKeeping(int which)
+{
+	bool cont = true;
+
+	// Actually create a new level
+	if(static_cast<int>(logLKeep.size()) >= options.newLevelInterval)
+	{
+		sort(logLKeep.begin(), logLKeep.end());
+		int ii = static_cast<int>(0.63212056
+				*static_cast<int>(logLKeep.size()));
+		LikelihoodType cutoff = logLKeep[ii];
+		std::cout<<std::setprecision(10);
+		std::cout<<"# Creating level "<<levels.size()
+			<<" with logL = "<<cutoff.logL
+			<<"."<<std::endl;
+		levels.push_back(Level(levels.back().get_logX() - 1., cutoff));
+
+		if(static_cast<int>(levels.size()) == options.maxNumLevels)
 		{
-			levels[i].accepts += samplers[j].levels[i].accepts - old[i].accepts;
-			levels[i].tries += samplers[j].levels[i].tries - old[i].tries;
-			levels[i].visits += samplers[j].levels[i].visits - old[i].visits;
-			levels[i].exceeds += samplers[j].levels[i].exceeds - old[i].exceeds;
+			logLKeep.clear();
+			Level::renormaliseVisits(levels, options.newLevelInterval);
+		}
+		else
+			logLKeep.erase(logLKeep.begin(), logLKeep.begin() + ii + 1);
+
+		Level::recalculateLogX(levels, options.newLevelInterval);
+		saveLevels();
+		deleteParticle();
+	}
+
+	if(count%options.saveInterval == 0)
+	{
+		saveParticle(which);
+		Level::recalculateLogX(levels, options.newLevelInterval);
+		saveLevels();
+		if(options.maxNumSamples > 0 &&
+			count/options.saveInterval == options.maxNumSamples)
+			cont = false;
+	}
+
+	return cont;
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::saveParticle(int which) const
+{
+	int N = count/options.saveInterval;
+	std::cout<<"# Saving a particle to disk. N = "<<N<<"."<<std::endl;
+
+	// Save the particle to file
+	std::fstream fout;
+	if(N == 1)
+	{
+		fout.open(options.sampleFile.c_str(), std::ios::out);
+		fout<<"# Samples file. One sample per line."<<std::endl;
+		fout<<"# "<<particles[0].description()<<std::endl;
+	}
+	else
+		fout.open(options.sampleFile.c_str(), std::ios::out|std::ios::app);
+	fout<<std::setprecision(10);
+	particles[which].print(fout); fout<<std::endl;
+	fout.close();
+
+	// Save the particle's info
+	if(N == 1)
+	{
+		fout.open(options.sampleInfoFile.c_str(), std::ios::out);
+		fout<<"# index, logLikelihood, tieBreaker, ID."<<std::endl;
+	}
+	else
+		fout.open(options.sampleInfoFile.c_str(), std::ios::out|std::ios::app);
+	fout<<std::setprecision(10);
+	fout<<indices[which]<<' '<<logL[which].logL<<' '
+			<<logL[which].tieBreaker<<' '<<which<<std::endl;
+	fout.close();
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::saveLevels() const
+{
+	std::fstream fout(options.levelsFile.c_str(), std::ios::out);
+	fout<<"# logX, logLikelihood, tieBreaker, accepts, tries, exceeds, visits."<<std::endl;
+	fout<<std::setprecision(10);
+	for(size_t i=0; i<levels.size(); i++)
+		fout<<levels[i]<<std::endl;
+	fout.close();
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::updateParticle(int which)
+{
+	// Copy the particle
+	ModelType proposal = particles[which];
+	LikelihoodType logL_proposal = logL[which];
+
+	// Perturb the proposal particle
+	double logH = proposal.perturb();
+	logL_proposal.logL = proposal.logLikelihood();
+	logL_proposal.tieBreaker += pow(10., 1.5 - 6.*randomU())*randn();
+	logL_proposal.tieBreaker = mod(logL_proposal.tieBreaker, 1.);
+	if(logH > 0.)
+		logH = 0.;
+
+	bool accepted = false;
+	if(levels[indices[which]].get_cutoff() < logL_proposal
+		&& randomU() <= exp(logH))
+	{
+		// Accept
+		particles[which] = proposal;
+		logL[which] = logL_proposal;
+		accepted = true;
+	}
+	levels[indices[which]].incrementTries(accepted);
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::updateIndex(int which)
+{
+	int proposedIndex = indices[which]
+		+ static_cast<int>(round(pow(10., 2.0*randomU())*randn()));
+
+	if(proposedIndex == indices[which])
+	{
+		proposedIndex = (randomU() < 0.5)?(indices[which]+1)
+				:(indices[which]-1);
+	}
+
+	if(proposedIndex < 0 ||
+		proposedIndex >= static_cast<int>(levels.size()))
+		return;
+
+	// Acceptance probability. logX part
+	double logA = levels[indices[which]].get_logX()
+			- levels[proposedIndex].get_logX();
+
+	// Pushing up part
+	logA += logPush(proposedIndex) - logPush(indices[which]);
+
+	// Enforce uniform exploration part (if all levels exist)
+	if(static_cast<int>(levels.size()) == options.maxNumLevels)
+		logA += options.beta*log((double)(levels[indices[which]].get_tries() + 1)/(double)(levels[proposedIndex].get_tries() + 1));
+
+	// Prevent exponentiation of huge numbers
+	if(logA > 0.)
+		logA = 0.;
+
+	if(randomU() <= exp(logA) && levels[proposedIndex].get_cutoff() < logL[which])
+	{
+		// Accept!
+		indices[which] = proposedIndex;
+	}
+}
+
+template<class ModelType>
+double MTSampler<ModelType>::logPush(int index) const
+{
+	assert(index >= 0 && index < static_cast<int>(levels.size()));
+	if(static_cast<int>(levels.size()) == options.maxNumLevels)
+		return 0.;
+
+	int i = index - (static_cast<int>(levels.size()) - 1);
+	return static_cast<double>(i)/options.lambda;
+}
+
+template<class ModelType>
+void MTSampler<ModelType>::deleteParticle()
+{
+	// Flag each particle as good or bad
+	std::vector<bool> good(options.numParticles, true);
+	int numBad = 0;
+	for(int i=0; i<options.numParticles; i++)
+	{
+		if(logPush(indices[i]) <= -5.)
+		{
+			good[i] = false;
+			numBad++;
 		}
 	}
 
-	for(size_t i=0; i<samplers.size(); i++)
-		samplers[i].levels = levels;
+	if(numBad <= options.numParticles)
+	{
+		// Replace bad particles with copies of good ones
+		for(int i=0; i<options.numParticles; i++)
+		{
+			if(!good[i])
+			{
+				int copy;
+				do
+				{
+					copy = randInt(options.numParticles);
+				}while(!good[copy]);
+
+				particles[i] = particles[copy];
+				indices[i] = indices[copy];
+				logL[i] = logL[copy];
+				std::cout<<"# Deleting a particle. Replacing"<<
+				" it with a copy of a good survivor."<<std::endl;
+			}
+		}
+	}
+	else
+		std::cerr<<"# Warning: all particles lagging! Very rare!"<<std::endl;
 }
 
 } // namespace DNest3
