c54cb602ea02bbc47c23dbe6c03d388f80ccf551
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-09-29 19:21:12 +1200

    Fix postprocess for one-parameter models

--- postprocess.py
+++ postprocess.py
@@ -37,6 +37,8 @@ def postprocess(temperature=1., numResampleLogX=1, plot=True, loaded=[], save=Tr
 		levels = np.atleast_2d(np.loadtxt("levels.txt"))
 		sample_info = np.atleast_2d(np.loadtxt("sample_info.txt"))
 		sample = np.atleast_2d(np.loadtxt("sample.txt"))
+		if(sample.shape[0] == 1):
+			sample = sample.T
 	else:
 		levels, sample_info, sample = loaded[0], loaded[1], loaded[2]
 
