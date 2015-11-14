6f0f8c2936f17d4bdc8f5976bc474ce5a624a356
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-14 19:25:24 +1300

    Python3 and Copyright statement

--- dnest_plots.py
+++ dnest_plots.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2009-2015 Brendon J. Brewer.
+#
+# This file is part of DNest3.
+#
+# DNest3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DNest3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DNest3. If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,7 +29,7 @@ def diffusion_plot():
 
 	ii = np.arange(1, sample_info.shape[0] + 1)
 
-	for i in xrange(0, ID.max() + 1):
+	for i in range(0, ID.max() + 1):
 		which = np.nonzero(ID == i)[0]
 		plt.plot(ii[which], j[which])
 
